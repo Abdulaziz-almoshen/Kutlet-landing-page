5082d1e6768e0313b3911b91d717337cb2950db4
Abdulaziz Almohsen <abdulaziz@exeque.co>
2022-01-08 01:42:14 +0300

    real menu added

--- pages/index.vue
+++ pages/index.vue
@@ -33,7 +33,7 @@
           <div
             class="h-56 md:h-80 bg-no-repeat bg-cover bg-center mb-8"
             style="
-              background-image: url('https://images.unsplash.com/photo-1576433477363-1a2a80cf9f60?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80');
+              background-image: url('https://pbs.twimg.com/media/FEF1HvKXoAA0zvX.jpg');
             "
           >
             <div
@@ -65,8 +65,8 @@
             >
               <div>
                 <img
-                  class="rounded-lg md:rounded-r-none"
-                  src="https://images.unsplash.com/photo-1550950158-d0d960dff51b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&h=500&q=60 "
+                  class="rounded-lg md:rounded-r-none h-30 w-30"
+                  src="/public/images/burger.jpeg"
                   alt="Hamburguesa "
                 />
               </div>
@@ -76,20 +76,19 @@
                   md:w-full md:shadow-none md:p-2 md:bg-transparent md:pt-5
                 "
               >
-                <h2 class="card-title md:text-lg">Classic Burger</h2>
+                <h2 class="card-title md:text-lg">Kutlet Burger</h2>
                 <p class="text-gray-700 leading-snug">
-                  Carne 100% de res, queso, lechuga, jitomate, salsa especial y
-                  pan con ajonjolí.
+                  
                 </p>
                 <div class="text-sm font-semibold text-gray-700 mt-2">
-                  75.00 SR
+                  34.00 SR
                 </div>
                 <div class="card-footer md:flex-col">
                   <div>
                     <span class="text-yellow-600"
                       >&starf;&starf;&starf;&starf;&star;</span
                     >
-                    <span class="text-gray-700">| 36 reseñas</span>
+                    <span class="text-gray-700">| 36 rating</span>
                   </div>
                   <div class="md:mt-4">
                     <span class="btn-ordenar">pick-up</span>
@@ -108,7 +107,7 @@
               <div>
                 <img
                   class="rounded-lg md:rounded-r-none"
-                  src="https://images.unsplash.com/photo-1585703900468-13c7a978ad86?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&h=500&q=80 "
+                  src="/public/images/sandwitch.jpeg"
                   alt="Alitas"
                 />
               </div>
@@ -118,21 +117,19 @@
                   md:shadow-none md:p-2 md:bg-transparent md:pt-5
                 "
               >
-                <h2 class="card-title md:text-lg">Wings</h2>
+                <h2 class="card-title md:text-lg">Phily steak</h2>
                 <p class="text-gray-700 leading-snug">
-                  Orden de 10 alitas de pollo fritas con salsa a tu elección.
                   <br />
-                  Tamarindo | BBQ | Cajún
                 </p>
                 <div class="text-sm font-semibold text-gray-700 mt-2">
-                  81.90 SR
+                  44.90 SR
                 </div>
                 <div class="card-footer md:flex-col">
                   <div>
                     <span class="text-yellow-600"
                       >&starf;&starf;&starf;&starf;&starf;</span
                     >
-                    <span class="text-gray-700">| 41 reseñas</span>
+                    <span class="text-gray-700">| 41 rating</span>
                   </div>
                   <div class="md:mt-4">
                     <span class="btn-ordenar">pick-up</span>
@@ -153,7 +150,7 @@
               <div>
                 <img
                   class="rounded-lg md:rounded-r-none"
-                  src="https://images.unsplash.com/photo-1572490122747-3968b75cc699?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&h=500&q=80 "
+                  src="/public/images/fries.jpeg"
                   alt="Malteada de Oreo"
                 />
               </div>
@@ -163,13 +160,12 @@
                   md:w-full md:shadow-none md:p-2 md:bg-transparent md:pt-5
                 "
               >
-                <h2 class="card-title md:text-lg">Milkshake Oreo</h2>
+                <h2 class="card-title md:text-lg">Kutlet fries</h2>
                 <p class="text-gray-700 leading-snug">
-                  Malteada de galletas oreo con leche de tu elección, decorada
-                  con galletas y crema batida.
+                  
                 </p>
                 <div class="text-sm font-semibold text-gray-700 mt-2">
-                  55.00 SR
+                  11.00 SR
                 </div>
                 <div class="card-footer md:flex-col">
                   <div>
@@ -194,8 +190,9 @@
             >
               <div>
                 <img
+                
                   class="rounded-lg md:rounded-r-none"
-                  src="https://images.unsplash.com/photo-1541592106381-b31e9677c0e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&h=500&q=80 "
+                  src="/public/images/soda-master.gif"
                   alt="Papas a la francesa"
                 />
               </div>
@@ -205,19 +202,18 @@
                   md:w-full md:shadow-none md:p-2 md:bg-transparent md:pt-5
                 "
               >
-                <h2 class="card-title md:text-lg">Fries</h2>
+                <h2 class="card-title md:text-lg">Soda</h2>
                 <p class="text-gray-700 leading-snug">
-                  Orden de papas a la francesa con nuestro sasonador especial.
                 </p>
                 <div class="text-sm font-semibold text-gray-700 mt-2">
-                  40.00 SR
+                  6.00 SR
                 </div>
                 <div class="card-footer md:flex-col">
                   <div>
                     <span class="text-yellow-600"
                       >&starf;&starf;&starf;&star;&star;</span
                     >
-                    <span class="text-gray-700">| 41 reseñas</span>
+                    <span class="text-gray-700">| 41 rating</span>
                   </div>
                   <div class="md:mt-4">
                     <span class="btn-ordenar">pick-up</span>
@@ -301,7 +297,6 @@
                 md:text-left md:flex md:justify-center
               "
             >
-            
               <div>
                 <span class="hidden md:inline">· </span>
                 <a class="hover:bg-yellow-500 p-1" href="# "
@@ -314,9 +309,9 @@
                 <a class="hover:bg-yellow-500 p-1" href="# "
                   >Phone: +966550433132</a
                 >
-                <a class="hover:bg-yellow-500 p-1" href="# "
-                  > Almojah Aldaimah Trading Company </a
-                >
+                <a class="hover:bg-yellow-500 p-1" href="# ">
+                  Almojah Aldaimah Trading Company
+                </a>
 
                 <span class="hidden md:inline"> ·</span>
               </div>
